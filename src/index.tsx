3e113697a8a04c80fd8e83d4b6b73456f103bc1c
rafael <rafaelmdeveloper@gmail.com>
2023-04-23 19:06:47 -0300

    #003

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer, Model } from 'miragejs'
+import { createServer, Model } from 'miragejs';
 
 createServer({
   models: {
-    transaction: Model, 
+    transaction: Model,
   },
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-      ],
-    })
+      transactions: [],
+    });
   },
-  
-  routes(){
-    this.namespace = 'api';
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
 
-    this.post('/api/transactions', (schema, request) => {
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
-    
-      return schema.create('transaction', data)
-    })
-  }
-})
+
+      return schema.create('transaction', data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
